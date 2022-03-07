73991177070d9b1edecd0e3584b6c4bef9ae8a63
luojia65 <me@luojia.cc>
2022-03-07 19:26:21 +0800

    Initial commit

---
+++ secure-app/src/main.rs
@@ -0,0 +1,20 @@
+#![no_std]
+#![no_main]
+
+// pick a panicking behavior
+use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
+// use panic_abort as _; // requires nightly
+// use panic_itm as _; // logs messages over ITM; requires ITM support
+// use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
+
+use cortex_m::asm;
+use cortex_m_rt::entry;
+
+#[entry]
+fn main() -> ! {
+    asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
+
+    loop {
+        // your code goes here
+    }
+}
