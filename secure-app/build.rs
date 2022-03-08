e01b1700d530c27d00c657ba24467e847893a701
luojia65 <me@luojia.cc>
2022-03-08 16:54:22 +0800

    Rearrange memory configuration
    Inline into build.rs scripts; remove memory.x files
    
    Signed-off-by: luojia65 <me@luojia.cc>

--- secure-app/build.rs
+++ secure-app/build.rs
@@ -1,31 +1,25 @@
-//! This build script copies the `memory.x` file from the crate root into
-//! a directory where the linker can always find it at build time.
-//! For many projects this is optional, as the linker always searches the
-//! project root directory -- wherever `Cargo.toml` is. However, if you
-//! are using a workspace or have a more complicated build setup, this
-//! build script becomes required. Additionally, by requesting that
-//! Cargo re-run the build script whenever `memory.x` is changed,
-//! updating `memory.x` ensures a rebuild of the application with the
-//! new memory settings.
+// Modified from cortex-m-quickstart
 
 use std::env;
 use std::fs::File;
 use std::io::Write;
 use std::path::PathBuf;
 
+/* ARM AN505 System */
+const AN505_MEMORY_SECURE: &'static [u8] = b"
+MEMORY
+{
+    FLASH : ORIGIN = 0x10000000, LENGTH = 2M
+    RAM : ORIGIN = 0x38000000, LENGTH = 2M
+}";
+
 fn main() {
-    // Put `memory.x` in our output directory and ensure it's
+    // Put memory configuration in our output directory and ensure it's
     // on the linker search path.
     let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
     File::create(out.join("memory.x"))
         .unwrap()
-        .write_all(include_bytes!("memory.x"))
+        .write_all(AN505_MEMORY_SECURE)
         .unwrap();
     println!("cargo:rustc-link-search={}", out.display());
-
-    // By default, Cargo will re-run a build script whenever
-    // any file in the project changes. By specifying `memory.x`
-    // here, we ensure the build script is only re-run when
-    // `memory.x` is changed.
-    println!("cargo:rerun-if-changed=memory.x");
 }
